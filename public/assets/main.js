3b56cc2cea091186e8542ede561fa02fa340e200
mateusgiordani <94706851+mateusgiordani@users.noreply.github.com>
2023-07-14 00:18:58 -0300

    primeira versao deploy ok

---
+++ public/assets/main.js
@@ -0,0 +1,25 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+// Initialize Swiper after your app is mounted
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
